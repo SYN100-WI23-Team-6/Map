1f201fd62c111bc5bb02ca9e48993f4713b6044f
JonathanYin <joyin@ucsd.edu>
2023-02-11 20:47:07 -0800

    Init express server setup

---
+++ server/index.js
@@ -0,0 +1,18 @@
+// import express, cors, and environment variables
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3000;
+
+// init express
+const app = express();
+
+// enable cors
+app.use(cors());
+
+// routes
+app.use("/api/search/", require("./routes/searchresults.js"));
+    
+app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
